6a605fe74661c61aed44f0ae69cc371f86b34fcf
felipe <felipe_herbert@hotmail.com.br>
2022-12-08 12:07:59 -0300

    Requisito 7 conclupido

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,11 @@
 const CategorySchema = (sequelize,DataTypes) => {
   const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type:DataTypes.STRING,
       allowNull: false,
